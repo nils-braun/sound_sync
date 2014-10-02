2e3f3cf0abf69e36f69bb7e7368de798aaf33655
Nils <area51.nils@googlemail.com>
2014-10-02 10:46:39 +0200

    Added doc strings!

--- try3/client.py
+++ try3/client.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python3
+"""
+This module starts the sender.
+"""
+
+__author__ = "nilpferd1991"
+__version__ = "1.0.0"
 
 import socket
+import subprocess
 import wave
-import math
 import time
 from clientBase import ClientBase
+import tempfile
 
 
 class ClientSender(ClientBase):
+    """
+    This class implements the sender to the server. It connects to the server,
+    sends the values of the frame rate and the waiting time to the server and
+    starts sending sound buffers to the sender.
+    """
     def __init__(self):
         ClientBase.__init__(self)
         self.read_values_from_file()
 
     def connect(self):
-        # connect to the server
+        """
+        Connect to the server and send frame rate and waiting time.
+        """
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((self.server_ip, self.port))
 
@@ -21,33 +35,54 @@ class ClientSender(ClientBase):
         self.client.sendall(b"sender")
         self.recv()
 
+        # Send the parameters to the server.
         self.send_values_to_server()
 
-    def message_loop(self):
-        while True:
-            f = wave.open("../test.wav", "rb")
-            for i in range(100):
-                buffer = bytearray(f.readframes(int(self.buffer_size/4.0)))
-                self.send(buffer)
-                time.sleep(self.waiting_time/1000.0)
-            f.close()
+    def message_loop(self, filename):
+        """
+        Start sending the sound buffers to the server in a loop.
+        In the moment these sound files come from a mp3 file which is converted to wav before using avconv.
+        """
+        _, tmp_file = tempfile.mkstemp()
 
-    def close(self):
-        self.client.close()
+        convert_command = ['avconv', '-y']
+        convert_command += ['-i', filename]
+        convert_command += ['-vn', '-f', 'wav', tmp_file]
 
-# Main function
-# Initialize a new instance of the client sender module
-# and connect to the server.
-# Then start the message loop (send parts of the file to the server)
-# until Ctrl-C is hit.
-if __name__ == "__main__":
+        ret_code = subprocess.call(convert_command, stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)
+        assert ret_code == 0
 
+        f = wave.open(tmp_file, "rb")
+        max_number = int(f.getnframes()/self.buffer_size*4.0)
+        for i in range(max_number):
+            buffer = bytearray(f.readframes(int(self.buffer_size/4.0)))
+            self.send(buffer)
+            time.sleep(self.waiting_time/1000.0)
+        f.close()
+
+
+def main():
+    """
+    Main function
+    Initialize a new instance of the client sender module
+    and connect to the server.
+    Then start the message loop (decode the file to wav and send it to the server)
+    until Ctrl-C is hit.
+    """
     client = ClientSender()
     client.connect()
 
+    # TODO: Get data not from file but from the currently playing sound!
+    # For this: switch the pulse-audio output to the alsa-loopback and collect this output via
+    # pcm = alsaaudio.PCM(type=alsaaudio.PCM_CAPTURE, card=u'hw:Loopback,1,0')
+    # pcm.read()
+
     try:
-        client.message_loop()
+        client.message_loop("/media/Daten/Music/In Flames/A Sense Of Purpose/02. Disconnected.mp3")
     except KeyboardInterrupt:
         pass
     finally:
         client.close()
+
+if __name__ == "__main__":
+    main()
--- try3/clientBase.py
+++ try3/clientBase.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
-try: import configparser
+"""
+This module implements the class ClientBase - a starting point for all client classes.
+"""
+
+__author__ = "nilpferd1991"
+__version__ = "1.0.0"
+
+try:
+    import configparser
 except ImportError:
     import ConfigParser as configparser
 import math
 
 
 class ClientBase:
+    """ class ClientBase
+    This class is the starting point for a listener and a sender client.
+    It introduces the needed variables like the server ip, the port and the client object,
+    but also the waiting time and the frame rate. The methods in this class supporting the crucial
+    messaging between the server and the client.
+    """
+
     def __init__(self):
         self.client = 0
         self.waiting_time = 0
@@ -19,12 +34,21 @@ class ClientBase:
         self.port = 50007
 
     def read_values_from_file(self):
+        """ DEPRICATED
+        :rtype: None
+        """
         # in ms
         self.waiting_time = int(self.config["DEFAULT"]["WaitingTime"])
         # in Hz
         self.frame_rate = int(self.config["DEFAULT"]["FrameRate"])
 
     def read_values_from_server(self):
+        """
+        Receive the frame rate and the waiting time from the server.
+
+        :rtype: None
+        """
+
         # First is frame rate in Hz
         self.frame_rate = int(self.recv())
         self.send_ok()
@@ -37,6 +61,12 @@ class ClientBase:
         self.set_buffer_size()
 
     def send_values_to_server(self):
+        """
+        Send the frame rate and the waiting time to the server.
+
+        :rtype : None
+        """
+
         # tell the server the frame rate in Hz
         self.send(str(self.frame_rate).encode())
         # get ok
@@ -46,13 +76,28 @@ class ClientBase:
         # get ok
         self.recv_ok()
 
+        # set the buffer size with the data
         self.set_buffer_size()
 
     def set_buffer_size(self):
-        # set the buffer_size correctly
+        """
+        Set the buffer_size to the number of bytes used for the sound snippet of length
+        waiting_time (now in seconds!) * frame_rate. Multiplication by 4 is used because of
+        format SE_16.
+
+        :rtype: None
+        """
+
         self.buffer_size = int(4*(2**math.log(self.waiting_time / 1000.0 * self.frame_rate, 2)))
 
     def recv_exact(self):
+        """
+        Receive exact buffer_size bytes from the server. If received less, repeat until the buffer
+        is full. If an error occurred, return nothing.
+
+        :rtype: bytearray
+        """
+
         pointer = 0
         tmp_buffer = bytearray(self.buffer_size)
         while pointer < self.buffer_size:
@@ -66,24 +111,43 @@ class ClientBase:
         return tmp_buffer
 
     def connect(self):
+        """
+        A function to connect to the server. Must be overwritten!
+        """
         pass
 
     def close(self):
+        """
+        Close the connection to the server if any.
+        """
         if self.client != 0:
             self.client.close()
 
     def send(self, message):
+        """
+        Send the message message to the server if any.
+        """
         if self.client != 0:
             self.client.sendall(message)
 
     def send_ok(self):
+        """
+        Send the message "ok" to the server of any.
+        """
         self.send("ok")
 
     def recv(self, buffer_size=None):
+        """
+        Receive a new message from the server of maximum length buffer_size. If not buffer_size is given,
+        the start_buffer_size is used.
+        """
         if buffer_size is None:
             buffer_size = self.start_buffer_size
         if self.client != 0:
             return self.client.recv(buffer_size)
 
     def recv_ok(self):
+        """
+        Receive a message without saving the message.
+        """
         self.recv(self.start_buffer_size)
