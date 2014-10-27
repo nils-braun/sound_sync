bd1b0a3682e28f59a820d1cd59b03bb586ad9f48
Nils <area51.nils@googlemail.com>
2014-10-27 23:54:17 +0100

    Testing

--- server.py
+++ server.py
@@ -116,6 +116,7 @@ class RequestHandler(SocketServer.BaseRequestHandler, ServerBase):
         try:
             new_sound_buffer = self.receive_buffer_with_exact_length()
             if new_sound_buffer:
+                print(new_sound_buffer[20])
                 RequestHandler.static_client_list.add_buffer(new_sound_buffer)
             else:
                 self.remove_sender()
