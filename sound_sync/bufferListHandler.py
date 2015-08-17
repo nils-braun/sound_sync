d3f364e82c006e058250dc87f18f61d1c8acc068
Nils <area51.nils@googlemail.com>
2015-08-17 23:01:28 +0200

    Started refactoring with bufferListHandler

---
+++ sound_sync/bufferListHandler.py
@@ -0,0 +1,59 @@
+from collections import deque
+from multiprocessing import Lock
+
+__author__ = 'nilpferd'
+
+
+class BufferListHandler:
+    """
+    Class for a typical ring buffer handling the sound buffer storage
+    on the server or on the listener clients.
+    """
+
+    def __init__(self, max_buffer_length=50):
+        """
+        Initialize with the max_buffer_length size
+        :param max_buffer_length int: The size of the buffer. If there are more elements
+         than space, the oldest elements are deleted.
+        """
+        self.max_buffer_length = max_buffer_length
+        self.buffers = deque()
+        self.start_buffer_index = 0
+        self.end_buffer_index = -1
+
+    def add_buffer(self, sound_buffer):
+        """
+        Add a buffer to the list.
+        :param sound_buffer T: The buffer to add
+        :return: None
+        """
+        mutex = Lock()
+        mutex.acquire()
+        self.buffers.append(sound_buffer)
+        self.end_buffer_index += 1
+
+        if len(self.buffers) > self.max_buffer_length:
+            self.buffers.popleft()
+            self.start_buffer_index += 1
+
+        mutex.release()
+
+    def is_empty(self):
+        """
+        Check if the list is empty
+        :return bool: if the list is empty
+        """
+        return len(self.buffers) == 0
+
+    def get_buffer_by_buffer_index(self, index):
+        """
+        Return the buffer with the given index or raise an exception if it can not be found
+        :param index int: the index of the buffer
+        :return T: the sound buffer
+        """
+
+        list_index = index - self.start_buffer_index
+        if list_index < 0 or list_index >= self.max_buffer_length:
+            raise IndexError
+        else:
+            return self.buffers[list_index]
---
+++ tests/test_bufferListHandler.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+__author__ = 'nils'
+
+
+class TestBufferListHandler(TestCase):
+    def test_add_buffer(self):
+        self.fail()
+
+    def test_is_empty(self):
+        self.fail()
+
+    def test_get_buffer_by_buffer_index(self):
+        self.fail()
