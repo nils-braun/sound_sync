f706512efdbd2a3156e6c5d7f6f25634ed78787e
Nils <area51.nils@googlemail.com>
2015-08-21 21:01:03 +0200

    Added buffer test

--- tests/test_bufferListHandler.py
+++ tests/test_bufferListHandler.py
@@ -1,14 +1,58 @@
 from unittest import TestCase
+from sound_sync.bufferListHandler import BufferListHandler
 
 __author__ = 'nils'
 
 
 class TestBufferListHandler(TestCase):
     def test_add_buffer(self):
-        self.fail()
+        buffer_list_handler = BufferListHandler(max_buffer_length=4)
+
+        buffer_list_handler.add_buffer("test0")
+        buffer_list_handler.add_buffer("test1")
+        buffer_list_handler.add_buffer("test2")
+        buffer_list_handler.add_buffer("test3")
+
+        self.assertEqual(buffer_list_handler.start_buffer_index, 0)
+        self.assertEqual(buffer_list_handler.end_buffer_index, 3)
+        self.assertEqual(len(buffer_list_handler.buffers), 4)
+
+        buffer_list_handler.add_buffer("test4")
+
+        self.assertEqual(buffer_list_handler.start_buffer_index, 1)
+        self.assertEqual(buffer_list_handler.end_buffer_index, 4)
+        self.assertEqual(len(buffer_list_handler.buffers), 4)
+
+        buffer_list_handler.add_buffer("test4")
+
+        self.assertEqual(buffer_list_handler.start_buffer_index, 2)
+        self.assertEqual(buffer_list_handler.end_buffer_index, 5)
+        self.assertEqual(len(buffer_list_handler.buffers), 4)
 
     def test_is_empty(self):
-        self.fail()
+        buffer_list_handler = BufferListHandler()
+
+        self.assertTrue(buffer_list_handler.is_empty())
+
+        buffer_list_handler.add_buffer("test")
+
+        self.assertFalse(buffer_list_handler.is_empty())
 
     def test_get_buffer_by_buffer_index(self):
-        self.fail()
+        buffer_list_handler = BufferListHandler(max_buffer_length=4)
+
+        buffer_list_handler.add_buffer("test0")
+        buffer_list_handler.add_buffer("test1")
+        buffer_list_handler.add_buffer("test2")
+        buffer_list_handler.add_buffer("test3")
+
+        self.assertEqual(buffer_list_handler.get_buffer_by_buffer_index(0), "test0")
+        self.assertEqual(buffer_list_handler.get_buffer_by_buffer_index(1), "test1")
+        self.assertEqual(buffer_list_handler.get_buffer_by_buffer_index(2), "test2")
+        self.assertEqual(buffer_list_handler.get_buffer_by_buffer_index(3), "test3")
+
+        buffer_list_handler.add_buffer("test4")
+        self.assertEqual(buffer_list_handler.get_buffer_by_buffer_index(4), "test4")
+
+        self.assertRaises(IndexError, buffer_list_handler.get_buffer_by_buffer_index, 0)
+        self.assertRaises(IndexError, buffer_list_handler.get_buffer_by_buffer_index, 5)
