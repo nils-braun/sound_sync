383c2887c1254c87105180a4e846ec4fa7355b43
Nils <area51.nils@googlemail.com>
2015-10-05 22:54:29 +0200

    Small changes

--- sound_sync/audio/sound_device.py
+++ sound_sync/audio/sound_device.py
@@ -5,5 +5,8 @@ class SoundDevice:
         self.frame_rate = None
         self.factor = None
 
+        self.added_delay = None
+        self.start_time = None
+
     def get_waiting_time(self):
         return float(self.factor) * float(self.buffer_size) / float(self.frame_rate)
