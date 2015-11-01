dac67e54e49762f65d408bcf63d0d45d46351028
Nils <area51.nils@googlemail.com>
2015-11-01 15:42:01 +0100

    Added first draft for listener main loop (untested)

--- sound_sync/clients/base.py
+++ sound_sync/clients/base.py
@@ -1,9 +1,14 @@
 import urllib
+try:
+    from sound_sync.buffer_server import BufferList
+except ImportError:
+    from buffer_server import BufferList
 
 from sound_sync.clients.connection import SoundSyncConnection
 from sound_sync.rest_server.server_items.json_pickable import JSONPickleable
 from sound_sync.rest_server.server_items.server_items import Client, Channel
 from sound_sync.timing.time_utils import get_current_date, waiting_time_to_datetime
+from sound_sync.timing.waitForTimeProcess import Timer
 
 
 class BaseListener(Client):
@@ -21,6 +26,15 @@ class BaseListener(Client):
 
         #: The player to send the data to
         self.player = None
+
+        #: The thread to handle the playing
+        self.play_thread = None
+
+        #: The list with the buffer from the server
+        self.buffer_list = BufferList(100)
+
+        #: The currently last played buffer number
+        self.last_played_buffer_number = None
 
     def initialize(self):
         if self.client_hash is not None:
@@ -59,11 +73,49 @@ class BaseListener(Client):
         if self.client_hash is None:
             raise AssertionError("Listener needs to be initialized first")
 
+        current_start_index = self.get_current_buffer_start_index()
+        self.buffer_list.set_start_index(current_start_index)
+
         # Receive as many packages as possible (to have a good starting point)
+        self.initial_fill_buffer_list()
 
         # Start the thread to put sound buffers in the audio queue
+        self.start_play_thread()
 
         # Receive information from the buffer server if possible
+        while True:
+            self.receive_and_add_next_buffer()
+
+    def start_play_thread(self):
+        next_play_time, next_buffer_number = self.calculate_next_starting_time_and_buffer()
+        waiting_time = self.player.get_waiting_time()
+        self.last_played_buffer_number = next_buffer_number - 1
+        self.play_thread = Timer(next_play_time, waiting_time, self.play_next_buffer)
+        self.play_thread.run()
+
+    def initial_fill_buffer_list(self):
+        # TODO: Maybe better: Do as many as possible
+        # noinspection PyArgumentList
+        current_start_index = self.buffer_list.get_start_index()
+        next_play_time, next_buffer_number = self.calculate_next_starting_time_and_buffer()
+        if next_buffer_number - current_start_index < 5:
+            raise ValueError("Too few buffers loaded into the server.")
+
+        for buffer_index in xrange(current_start_index, next_buffer_number - 1):
+            self.receive_and_add_next_buffer()
+
+    def receive_and_add_next_buffer(self):
+        # noinspection PyArgumentList
+        next_buffer_number = self.buffer_list.get_next_free_index()
+        temp_buffer = self.get_buffer(next_buffer_number)
+        self.buffer_list.add_buffer(temp_buffer)
+
+    def play_next_buffer(self):
+        next_buffer_number = self.last_played_buffer_number + 1
+        next_buffer = self.buffer_list.get_buffer(str(next_buffer_number))
+        self.player.put(next_buffer)
+
+        self.last_played_buffer_number = next_buffer_number
 
     def calculate_next_starting_time_and_buffer(self):
         current_time = get_current_date()
@@ -81,6 +133,20 @@ class BaseListener(Client):
         next_time = start_time + waiting_time_to_datetime(number_of_next_clock * waiting_time)
 
         return next_time, number_of_next_clock
+
+    def get_current_buffer_start_index(self):
+        response = self.connection.http_client.fetch(self.handler_string + "/start")
+        if response.code == 200:
+            return int(response.body)
+        else:
+            raise RuntimeError(response)
+
+    def get_buffer(self, buffer_number):
+        response = self.connection.http_client.fetch(self.handler_string + "/get/%d" % buffer_number)
+        if response.code == 200:
+            return response.body
+        else:
+            raise RuntimeError(response)
 
 
 class BaseSender(Channel):
