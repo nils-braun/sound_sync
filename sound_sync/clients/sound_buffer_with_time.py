b64cdb794d4bc1d9cbe1ae6bd4ffd78667ed0c79
Nils <area51.nils@googlemail.com>
2015-12-28 22:41:39 +0100

    Finished test for sound buffer with time

--- sound_sync/clients/sound_buffer_with_time.py
+++ sound_sync/clients/sound_buffer_with_time.py
@@ -9,20 +9,25 @@ class SoundBufferWithTime():
         self.buffer_time = buffer_time
         self.buffer_number = buffer_number
         self.sound_buffer_length = len(sound_buffer)
+        print self.sound_buffer
 
-    def construct_from_string(self, string):
+    @staticmethod
+    def construct_from_string(string):
         splitted_information = string.split(SoundBufferWithTime.SPLIT_SEPARATOR)
 
-        self.sound_buffer_length = int(splitted_information[-1])
-        self.buffer_number = int(splitted_information[-2])
-        self.buffer_time = to_datetime(splitted_information[-3])
-        self.sound_buffer = "".join(splitted_information[:-4])
+        new_sound_buffer = SoundBufferWithTime(
+            sound_buffer=SoundBufferWithTime.SPLIT_SEPARATOR.join(splitted_information[:-3]),
+            buffer_number=int(splitted_information[-2]),
+            buffer_time=to_datetime(splitted_information[-3]))
 
-        assert len(self.sound_buffer) == self.sound_buffer_length
+        sound_buffer_length = int(splitted_information[-1])
+        assert new_sound_buffer.sound_buffer_length == sound_buffer_length
+
+        return new_sound_buffer
 
     def to_string(self):
         return_string = SoundBufferWithTime.SPLIT_SEPARATOR.join([self.sound_buffer,
-                                                                  str(self.buffer_time), 
+                                                                  str(self.buffer_time),
                                                                   str(self.buffer_number),
                                                                   str(self.sound_buffer_length)])
 
--- tests/test_clients/test_soundBufferWithTime.py
+++ tests/test_clients/test_soundBufferWithTime.py
@@ -7,17 +7,22 @@ from sound_sync.clients.sound_buffer_with_time import SoundBufferWithTime
 
 class TestSoundBufferWithTime(TestCase):
     def test_construct_from_string(self):
-        self.fail()
+        test_string = "This is<||>a test buf<||>2015-11-27 16:12:11.102030<||>42<||>21"
+        new_sound_buffer = SoundBufferWithTime.construct_from_string(test_string)
+
+        self.assertEqual(new_sound_buffer.sound_buffer_length, 21)
+        self.assertEqual(new_sound_buffer.sound_buffer, "This is<||>a test buf")
+        self.assertEqual(new_sound_buffer.buffer_number, 42)
+        self.assertEqual(new_sound_buffer.buffer_time, datetime.datetime(2015, 11, 27, 16, 12, 11, 102030))
 
     def test_to_string(self):
-        test_sound_buffer = "This is a test buffer"
-        test_sound_buffer_length = len(test_sound_buffer)
+        test_sound_buffer = "This is<||>a test buf"
         test_buffer_number = 42
         test_buffer_time = datetime.datetime(2015, 11, 27, 16, 12, 11, 102030)
 
         test_object = SoundBufferWithTime(sound_buffer=test_sound_buffer, buffer_number=test_buffer_number,
                                           buffer_time=test_buffer_time)
 
-        expected_string = "This is a test buffer<||>2015-11-27 16:12:11.102030<||>42<||>21"
+        expected_string = "This is<||>a test buf<||>2015-11-27 16:12:11.102030<||>42<||>21"
 
         self.assertEqual(test_object.to_string(), expected_string)
