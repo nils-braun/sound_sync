ef5e4003265042488efdb5a8f3354ac6bd96ce2e
Nils Braun <nils.braun@kit.edu>
2017-01-18 21:04:57 +0100

    Added also a test for the ring buffer

--- tests/test_utils/test_soundBufferList.py
+++ tests/test_utils/test_soundBufferList.py
@@ -1,11 +1,11 @@
 from unittest import TestCase
-from sound_sync.entities.buffer_list import OrderedBufferList
+from sound_sync.entities.buffer_list import OrderedBufferList, RingBufferList
 
 from sound_sync.entities.sound_buffer_with_time import SoundBufferWithTime
 from sound_sync.timing.time_utils import get_current_date
 
 
-class TestSoundBufferList(TestCase):
+class TestOrderedBufferList(TestCase):
     def setUp(self):
         self.max_buffer_size = 5
         self.buffer_list = OrderedBufferList(self.max_buffer_size)
@@ -17,9 +17,11 @@ class TestSoundBufferList(TestCase):
         self.buffer_list.append(self.buffer_examples[1])
 
         self.assertEqual(self.buffer_list.pop().buffer_number, 0)
+        self.assertEqual(self.buffer_list.glimpse().buffer_number, 1)
         self.assertEqual(self.buffer_list.pop().buffer_number, 1)
 
         self.assertRaises(IndexError, self.buffer_list.pop)
+        self.assertRaises(IndexError, self.buffer_list.glimpse)
 
     def test_doubled_store_and_receive(self):
         self.buffer_list.append(self.buffer_examples[0])
@@ -30,15 +32,18 @@ class TestSoundBufferList(TestCase):
         self.buffer_list.append(self.buffer_examples[2])
 
         self.assertEqual(self.buffer_list.pop().buffer_number, 0)
+        self.assertEqual(self.buffer_list.glimpse().buffer_number, 1)
         self.assertEqual(self.buffer_list.pop().buffer_number, 1)
         self.assertEqual(self.buffer_list.pop().buffer_number, 2)
 
         self.assertRaises(IndexError, self.buffer_list.pop)
+        self.assertRaises(IndexError, self.buffer_list.glimpse)
 
     def test_store_and_receive_mixed(self):
         self.buffer_list.append(self.buffer_examples[1])
         self.buffer_list.append(self.buffer_examples[2])
 
+        self.assertEqual(self.buffer_list.glimpse().buffer_number, 1)
         self.assertEqual(self.buffer_list.pop().buffer_number, 1)
 
         # will be below first item -> should be dismissed
@@ -46,14 +51,17 @@ class TestSoundBufferList(TestCase):
         self.buffer_list.append(self.buffer_examples[2])
         self.buffer_list.append(self.buffer_examples[3])
 
+        self.assertEqual(self.buffer_list.glimpse().buffer_number, 2)
         self.assertEqual(self.buffer_list.pop().buffer_number, 2)
 
         self.buffer_list.append(self.buffer_examples[4])
 
         self.assertEqual(self.buffer_list.pop().buffer_number, 3)
+        self.assertEqual(self.buffer_list.glimpse().buffer_number, 4)
         self.assertEqual(self.buffer_list.pop().buffer_number, 4)
 
         self.assertRaises(IndexError, self.buffer_list.pop)
+        self.assertRaises(IndexError, self.buffer_list.glimpse)
 
     def test_is_continuous(self):
         self.buffer_list.append(self.buffer_examples[2])
@@ -79,3 +87,38 @@ class TestSoundBufferList(TestCase):
         self.buffer_list.append(self.buffer_examples[3])
 
         self.assertRaises(IndexError, self.buffer_list.append, self.buffer_examples[9])
+
+class RingBufferTestCase(TestCase):
+    def test_normal_operation(self):
+        buffer = RingBufferList(5)
+
+        self.assertRaises(IndexError, lambda: buffer[0])
+
+        buffer.append("1")
+
+        self.assertEqual(buffer[0], "1")
+
+        self.assertRaises(IndexError, lambda: buffer[1])
+        self.assertRaises(IndexError, lambda: buffer[11])
+
+    def test_buffer_goes_round(self):
+        buffer = RingBufferList(3)
+
+        buffer.append("1")
+        buffer.append("2")
+        buffer.append("3")
+        buffer.append("4")
+
+        self.assertEqual(buffer[0], "2")
+        self.assertEqual(buffer[1], "3")
+        self.assertEqual(buffer[2], "4")
+
+    def test_buffer_overflow(self):
+        buffer = RingBufferList(100)
+
+        for i in range(100):
+            buffer.append(i)
+
+        self.assertEqual(buffer[99], 99)
+
+        self.assertRaises(IndexError, lambda: buffer[100])
